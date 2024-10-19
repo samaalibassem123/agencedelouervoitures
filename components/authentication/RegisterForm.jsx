a1afff3fd9cc055ba175ee32162b632091cda31b
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-10-19 22:59:25 +0200

    c4

--- components/authentication/RegisterForm.jsx
+++ components/authentication/RegisterForm.jsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/input-otp";
 import { motion } from "framer-motion";
 import { AnimateShowElement } from "@/utils/animation";
+import { ScrollArea } from "../ui/scroll-area";
 
 export default function RegisterForm() {
   return (
@@ -31,137 +32,142 @@ export default function RegisterForm() {
       </div>
 
       {/*left side */}
-      <div class=" md:hidden absolute inset-0 -z-10 h-svh w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
-
-      <motion.div
-        variants={AnimateShowElement}
-        initial="hidden"
-        animate="visible"
-        className="flex flex-col justify-center gap-5 h-svh w-full border md:p-10 rounded-lg "
-      >
+      <div class="md:hidden absolute inset-0 -z-10 h-svh w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
+      <ScrollArea className="h-svh">
         <motion.div
           variants={AnimateShowElement}
-          className="flex flex-col gap-2 md:h-svh md:justify-around"
+          initial="hidden"
+          animate="visible"
+          className="flex flex-col justify-center gap-5 h-svh w-full border md:p-10 rounded-lg "
         >
-          {/*text appear with the vd*/}
           <motion.div
             variants={AnimateShowElement}
-            className="md:flex md:flex-col hidden"
+            className="flex flex-col gap-2 md:h-svh md:justify-around"
           >
-            <h1 className="text-2xl font-semibold">
-              Full Self-Driving (Supervised)
-            </h1>
-            <p className="text-sm">
-              Vous pourrez expérimenter la conduite entièrement autonome
-              (supervisée). Sous votre supervision, le véhicule Tesla naviguera
-              automatiquement dans les virages, les intersections et plus encore
-              le long de votre itinéraire. Un conseiller Tesla peut vous aider à
-              vous installer et vous accompagner pendant le trajet sur demande.
-            </p>
-          </motion.div>
-
-          <motion.div
-            variants={AnimateShowElement}
-            className="flex flex-col gap-2"
-          >
-            {/*Register with credentials*/}
-            <form
-              action=""
-              className="flex flex-col gap-5 bg-white rounded-lg p-5 "
+            {/*text appear with the vd*/}
+            <motion.div
+              variants={AnimateShowElement}
+              className="md:flex md:flex-col hidden"
             >
-              <h1 className="text-2xl font-semibold ">Register</h1>
-              <hr />
-              <div className="flex w-full gap-3">
-                <div>
-                  <Label>Nom:</Label>
-                  <Input placeholder="donnez votre nom" name="username" />
-                </div>
-                <div>
-                  <Label>Prénom:</Label>
-                  <Input placeholder="donnez  votre prénom" name="userFname" />
-                </div>
-              </div>
-              <div className="flex flex-col gap-2">
-                <Label>Numero Télephone:</Label>
-                <div className="w-full" name="tel">
-                  <InputOTP maxLength={8}>
-                    <InputOTPGroup>
-                      <InputOTPSlot index={0} />
-                      <InputOTPSlot index={1} />
-                    </InputOTPGroup>
-                    <InputOTPSeparator />
+              <h1 className="text-2xl font-semibold">
+                Full Self-Driving (Supervised)
+              </h1>
+              <p className="text-sm">
+                Vous pourrez expérimenter la conduite entièrement autonome
+                (supervisée). Sous votre supervision, le véhicule Tesla
+                naviguera automatiquement dans les virages, les intersections et
+                plus encore le long de votre itinéraire. Un conseiller Tesla
+                peut vous aider à vous installer et vous accompagner pendant le
+                trajet sur demande.
+              </p>
+            </motion.div>
 
-                    <InputOTPGroup>
-                      <InputOTPSlot index={2} />
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                    </InputOTPGroup>
-
-                    <InputOTPSeparator />
-
-                    <InputOTPGroup>
-                      <InputOTPSlot index={5} />
-                      <InputOTPSlot index={6} />
-                      <InputOTPSlot index={7} />
-                    </InputOTPGroup>
-                  </InputOTP>
-                </div>
-              </div>
-              <div>
-                <Label>Mail:</Label>
-                <Input
-                  placeholder="example@gmail.com"
-                  name="mail"
-                  type="email"
-                  required
-                />
-              </div>
-              <div>
-                <Label>Password:</Label>
-                <Input
-                  placeholder="**************"
-                  type="password"
-                  required
-                  name="password"
-                />
-              </div>
-              <div>
-                <Label>Confirm Password:</Label>
-                <Input
-                  placeholder="**************"
-                  type="password"
-                  required
-                  name="cpassword"
-                />
-              </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit">Register</Button>
-              </div>
-            </form>
-            {/*login with google*/}
-            <form
-              action=""
-              className="flex items-center p-1 justify-center w-full bg-white"
+            <motion.div
+              variants={AnimateShowElement}
+              className="flex flex-col gap-2"
             >
-              <Button
-                className="md:w-full  border border-black"
-                variant="primary"
+              {/*Register with credentials*/}
+              <form
+                action=""
+                className="flex flex-col gap-5 bg-white rounded-lg p-5 "
               >
-                <div className="flex items-center gap-2">
-                  <span>Continue avec Google</span> <FcGoogle />
+                <h1 className="text-2xl font-semibold ">Register</h1>
+                <hr />
+                <div className="flex w-full gap-3">
+                  <div>
+                    <Label>Nom:</Label>
+                    <Input placeholder="donnez votre nom" name="username" />
+                  </div>
+                  <div>
+                    <Label>Prénom:</Label>
+                    <Input
+                      placeholder="donnez  votre prénom"
+                      name="userFname"
+                    />
+                  </div>
                 </div>
-              </Button>
-            </form>
-          </motion.div>
+                <div className="flex flex-col gap-2">
+                  <Label>Numero Télephone:</Label>
+                  <div className="w-full">
+                    <InputOTP maxLength={8} name="tel">
+                      <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                      </InputOTPGroup>
+                      <InputOTPSeparator />
 
-          {/*redirect to login */}
-          <motion.div variants={AnimateShowElement}>
-            <Link href={"/login"} className="underline bg-white p-2">
-              Connecter avec votre compte
-            </Link>
+                      <InputOTPGroup>
+                        <InputOTPSlot index={2} />
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                      </InputOTPGroup>
+
+                      <InputOTPSeparator />
+
+                      <InputOTPGroup>
+                        <InputOTPSlot index={5} />
+                        <InputOTPSlot index={6} />
+                        <InputOTPSlot index={7} />
+                      </InputOTPGroup>
+                    </InputOTP>
+                  </div>
+                </div>
+                <div>
+                  <Label>Mail:</Label>
+                  <Input
+                    placeholder="example@gmail.com"
+                    name="mail"
+                    type="email"
+                    required
+                  />
+                </div>
+                <div>
+                  <Label>Password:</Label>
+                  <Input
+                    placeholder="**************"
+                    type="password"
+                    required
+                    name="password"
+                  />
+                </div>
+                <div>
+                  <Label>Confirm Password:</Label>
+                  <Input
+                    placeholder="**************"
+                    type="password"
+                    required
+                    name="cpassword"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button type="submit">Register</Button>
+                </div>
+              </form>
+              {/*login with google*/}
+              <form
+                action=""
+                className="flex items-center p-1 justify-center w-full bg-white"
+              >
+                <Button
+                  className="md:w-full  border border-black"
+                  variant="primary"
+                >
+                  <div className="flex items-center gap-2">
+                    <span>Continue avec Google</span> <FcGoogle />
+                  </div>
+                </Button>
+              </form>
+            </motion.div>
+
+            {/*redirect to login */}
+            <motion.div variants={AnimateShowElement}>
+              <Link href={"/login"} className="underline bg-white p-2">
+                Connecter avec votre compte
+              </Link>
+            </motion.div>
           </motion.div>
-        </motion.div>
-      </motion.div>
+        </motion.div>{" "}
+      </ScrollArea>
     </div>
   );
 }
